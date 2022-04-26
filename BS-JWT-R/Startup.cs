d7bd05e019ceed797500eb0394593995111e2405
calvinchang <q20274982@gmail.com>
2022-04-26 09:34:38 +0800

    register jwt to startup

--- BS-JWT-R/Startup.cs
+++ BS-JWT-R/Startup.cs
@@ -1,14 +1,17 @@
 using BS_JWT_R.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BS_JWT_R
@@ -62,6 +65,40 @@ namespace BS_JWT_R
             });
 
             services.AddTransient<JwtHelper>();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+                    options.IncludeErrorDetails = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+
+                        // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                        // 一般我們都會驗證 Issuer
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                        // 通常不太需要驗證 Audience
+                        ValidateAudience = false,
+                        //ValidAudience = "JwtAuthDemo", // 不驗證就不需要填寫
+
+                        // 一般我們都會驗證 Token 的有效期間
+                        ValidateLifetime = true,
+
+                        // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                        ValidateIssuerSigningKey = false,
+
+                        // "1234567890123456" 應該從 IConfiguration 取得
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,6 +121,8 @@ namespace BS_JWT_R
 
             app.UseRouting();
 
+            //先驗證、再授權
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
