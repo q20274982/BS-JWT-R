fe721ddc3aab505c3a6e2bb9e1a48a2f3072a37d
calvinchang <q20274982@gmail.com>
2022-04-26 09:27:10 +0800

    tokencontroller v1

--- BS-JWT-R/APIControllers/TokenController.cs
+++ BS-JWT-R/APIControllers/TokenController.cs
@@ -1,6 +1,9 @@
 ﻿using BS_JWT_R.Helpers;
+using BS_JWT_R.Models.VM;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace BS_JWT_R.APIControllers
 {
@@ -15,20 +18,32 @@ namespace BS_JWT_R.APIControllers
             _jwt = jwt;
         }
 
-        public IActionResult SignIn()
+        [HttpPost("signin")]
+        public IActionResult SignIn(LoginVM query)
         {
-            
+            if (ValidateUser(query))
+            {
+                return Ok(_jwt.GenerateToken(query.username));
+            }
+
+            return BadRequest();
         }
 
+        [HttpGet("claims")]
         public IActionResult GetClaims()
         {
-
+            return Ok(User.Claims.Select(x => new { x.Type, x.Value }));
         }
 
+        [HttpGet("username")]
         public IActionResult GetUserName()
         {
-
+            return Ok(User.Identity.Name);
         }
 
+        private bool ValidateUser(LoginVM query)
+        {
+            return true;
+        }
     }
 }
---
+++ BS-JWT-R/Models/VM/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace BS_JWT_R.Models.VM
+{
+    public class LoginVM
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
