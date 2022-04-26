c620318d1ab612fae4655639ecf83f7ec2a0e780
calvinchang <q20274982@gmail.com>
2022-04-26 08:45:46 +0800

    加入專案檔案。

---
+++ BS-JWT-R/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BS_JWT_R.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
