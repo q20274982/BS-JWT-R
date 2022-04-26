b28021d010355aa1cc9fbac7facc1011edaedc48
calvinchang <q20274982@gmail.com>
2022-04-26 09:16:52 +0800

    註冊

--- BS-JWT-R/Startup.cs
+++ BS-JWT-R/Startup.cs
@@ -1,3 +1,4 @@
+using BS_JWT_R.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace BS_JWT_R
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "bs_JWT", Version = "v1"});
             });
+
+            services.AddTransient<JwtHelper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
