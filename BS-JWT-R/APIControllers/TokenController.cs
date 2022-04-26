eba58a01d93f55975ebad6d5282b19b06b252b93
calvinchang <q20274982@gmail.com>
2022-04-26 09:21:29 +0800

    define TokenController endpoint

---
+++ BS-JWT-R/APIControllers/TokenController.cs
@@ -0,0 +1,34 @@
+﻿using BS_JWT_R.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BS_JWT_R.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelper _jwt;
+
+        public TokenController(JwtHelper jwt)
+        {
+            _jwt = jwt;
+        }
+
+        public IActionResult SignIn()
+        {
+            
+        }
+
+        public IActionResult GetClaims()
+        {
+
+        }
+
+        public IActionResult GetUserName()
+        {
+
+        }
+
+    }
+}
